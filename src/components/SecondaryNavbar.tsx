11016025a2dbafd0d29908b8f7a8af06068c2f06
paras594 <parasarora594@gmail.com>
2025-05-27 10:54:25 +0530

    fix auth issue in secondary nav

--- src/components/SecondaryNavbar.tsx
+++ src/components/SecondaryNavbar.tsx
@@ -2,14 +2,13 @@
 import ProfileDropdown from "@/components/ProfileDropdown";
 import Link from "next/link";
 import ConnectedDevicesCount from "@/components/ConnectedDevicesCount";
-import { getServerAuthSession } from "@/lib/authOptions";
 import { RiArrowLeftLine } from "react-icons/ri";
-import { useSession } from "next-auth/react";
+import { useAuth } from "@/contexts/auth-context";
 
 const loggedIn = false;
 
 const SecondaryNavbar = () => {
-  const { data: authSession, status } = useSession();
+  const { user, loading } = useAuth();
 
   return (
     <div className="navbar border-b">
@@ -18,14 +17,14 @@ const SecondaryNavbar = () => {
           <RiArrowLeftLine />
         </Link>
       </div>
-      {status === "loading" ? (
+      {loading ? (
         <span className="loading loading-spinner text-gray-300 loading-md mr-2 md:mr-4"></span>
       ) : (
         <div className="flex-none gap-2 mr-2 md:mr-4">
-          {authSession?.user ? (
+          {user ? (
             <div className="flex gap-4 md:gap-6 items-center">
               <ConnectedDevicesCount />
-              <ProfileDropdown name={authSession?.user?.name} />
+              <ProfileDropdown name={user.displayName} />
             </div>
           ) : (
             <Link href="/login" className="btn">
