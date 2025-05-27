9d32d93186ea60443a3a651cb8531a3e664a1631
Paras <38758886+paras594@users.noreply.github.com>
2025-05-27 10:51:23 +0530

    Feat/v2 (#2)
    * integrates firebase auth in login, register and navbar
    
    * migrate to firebase auth
    
    * removes nextauth

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,6 +10,7 @@ const Login = () => {
         <h1 className="prose-2xl font-bold text-center mb-8 mt-12">Login</h1>
         {/* <LoginForm /> */}
         <FirebaseLoginForm />
+        <p className="text-center mt-20 text-gray-300">v2.0</p>
       </Container>
     </div>
   );
